23d0fad08993110938090411fef73b4147ab51f1
Vadim Kasatkin <penguin2804@gmail.com>
2022-05-06 17:58:16 +0400

    refactor: change to DB instance

--- console.php
+++ console.php
@@ -1,21 +1,7 @@
 <?php
 
 require './Migration.php';
+require './DB.php';
 
-$host = '127.0.0.1';
-$db   = 'tasks';
-$user = 'root';
-$pass = '';
-$charset = 'utf8';
-
-$opt = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-$pdo = new PDO($dsn, $user, $pass, $opt);
-$migration = new Migration($pdo);
+$migration = new Migration(DB::getInstance());
 $migration->migrate();
