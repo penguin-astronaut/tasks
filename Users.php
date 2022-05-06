413b2cabac50ca51fde272274a9cd0f44301beec
Vadim Kasatkin <penguin2804@gmail.com>
2022-05-06 18:16:19 +0400

    refactor: users rename methods and new method for get user id

--- Users.php
+++ Users.php
@@ -3,31 +3,80 @@
 class Users {
     private PDO $db;
 
+    public const STATUS_SUCCESS = 1;
+    public const STATUS_ERROR = 2;
+
     public function __construct(PDO $db)
     {
         $this->db = $db;
     }
 
-    public function register(string $login, string $password): int
+    public function create(string $login, string $password): array
     {
-        $login = trim($login);
+        $login = htmlspecialchars(trim($login));
+        $password = htmlspecialchars(trim($password));
+
+        if (!$login || !$password) {
+            return [
+                'status' => self::STATUS_ERROR,
+                'message' => 'Login and password required!'
+            ];
+        }
+
         $passwordHashed = password_hash($password, PASSWORD_BCRYPT);
+
+        $userId = $this->getUserId($login, $passwordHashed);
+        if ($userId) {
+            return [
+                'status' => self::STATUS_ERROR,
+                'message' => 'User with that login exist'
+            ];
+        }
 
         $sql = "INSERT INTO users (login, password) VALUES (?,?)";
         $stmt = $this->db->prepare($sql);
         $stmt->execute([$login, $passwordHashed]);
 
-        return $this->db->lastInsertId();
+        return [
+            'status' => self::STATUS_SUCCESS,
+            'user_id' => $this->db->lastInsertId()
+        ];
     }
 
-    public function login(string $login, string $password): int
+    public function get(string $login, string $password): array
     {
-        $login = trim($login);
+        $login = htmlspecialchars(trim($login));
+        $password = htmlspecialchars(trim($password));
+
+        if (!$login || !$password) {
+            return [
+                'status' => self::STATUS_ERROR,
+                'message' => 'Login and password required!'
+            ];
+        }
+
         $passwordHashed = password_hash($password, PASSWORD_BCRYPT);
 
+        $id = $this->getUserId($login, $passwordHashed);
+
+        if (!$id) {
+            return [
+                'status' => self::STATUS_ERROR,
+                'message' => 'user not found'
+            ];
+        }
+
+        return [
+            'status' => self::STATUS_SUCCESS,
+            'user_id' => $id
+        ];
+    }
+
+    private function getUserId(string $login, $password): int
+    {
         $sql = "SELECT id FROM users WHERE login=? AND password=?";
         $stmt = $this->db->prepare($sql);
-        $stmt->execute([$login, $passwordHashed]);
+        $stmt->execute([$login, $password]);
 
         return $stmt->fetchColumn();
     }
