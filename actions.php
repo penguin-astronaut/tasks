32aafc8e3a130ffa4e6a5423edc08201a9e267d8
Vadim Kasatkin <penguin2804@gmail.com>
2022-05-07 14:00:59 +0400

    feat: actions for tasks

---
+++ actions.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+require 'Tasks.php';
+require 'DB.php';
+
+$tasks = new Tasks(DB::getInstance());
+
+$action = $_GET['action'];
+
+switch ($action) {
+    case 'remove':
+        if (isset($_GET['id'])) {
+            $tasks->remove($_GET['id'], $_SESSION['user']);
+        }
+        break;
+    case 'remove_all':
+        $tasks->removeAll($_SESSION['user']);
+        break;
+    case 'change_status':
+        if (isset($_GET['status'])) {
+            $status = $_GET['status'] === 'ready' ? Tasks::STATUS_READY : Tasks::STATUS_UNREADY;
+            $tasks->changeStatus($_GET['id'], $_SESSION['user'],$status);
+        }
+        break;
+    case 'ready_all':
+        $tasks->readyAll($_SESSION['user']);
+        break;
+}
+
+header('Location: /');
